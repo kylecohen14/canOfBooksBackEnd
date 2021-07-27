62386c41df360c618544028d04d1307a48c8e4b3
kylec <KYLECOHEN14@GMAIL.COM>
2021-07-27 10:39:45 -0400

    added to server

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 'use strict';
 
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 
 const app = express();
-app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.use(cors());
+app.use(express.json());
+
+const client = jwksClient({
+  jwksUri: 'https://dev-6xlimb1s.us.auth0.com'
+  // extension on this Uri???
+});
+
+// function getKey(header)
+
+
+app.get('/auth-test', (req, res) => {
 
   // TODO: 
   // STEP 1: get the jwt from the headers
@@ -19,6 +30,19 @@ app.get('/test', (request, response) => {
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
-})
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+  const token = req.headers.authorization.split(' ')[1];
+
+  jwt.verify(token, getKey, {}, function(err, user) {
+    if (err) {
+      res.send('invalid token - you cannot access this route');
+    } else {
+      res.json({ 'token': token })
+    }
+});
+});
+  
+
+app.listen(PORT, () => {
+console.log(`listening on ${PORT}`);
+});
